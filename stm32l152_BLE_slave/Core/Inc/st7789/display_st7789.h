38d6e04ee0781534926fbef882690a8c483826c3
Андрианов Виталий <andrianov.vital@yandex.ru>
2024-02-13 15:19:30 +0300

    st7789 display params #15

---
+++ stm32l152_BLE_slave/Core/Inc/st7789/display_st7789.h
@@ -0,0 +1,21 @@
+/*
+ * display_st7789.h
+ *
+ *  Created on: Feb 2, 2024
+ *      Author: Andrianov Vitaly
+ */
+
+#ifndef INC_UTILS_DISPLAY_ST7789_H_
+#define INC_UTILS_DISPLAY_ST7789_H_
+
+#include "st7789/display_st7789.h"
+
+//-------------- Параметры дисплея --------------//
+#define		WIDTH_st7789	240
+#define 	HEIGHT_st7789	320
+#define 	XSTART_st7789	0
+#define 	YSTART_st7789	0
+#define 	ROTATION_st7789	(MADCTL_RGB)
+//-----------------------------------------------//
+
+#endif /* INC_UTILS_DISPLAY_ST7789_H_ */
